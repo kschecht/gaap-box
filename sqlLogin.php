cbca1b0167b17fc9be314d1d9d1d33e26c99f73a
Kevin Schechter <kschechter@rocketmail.com>
2018-05-25 20:32:30 -0400

    database adding functionality

---
+++ sqlLogin.php
@@ -0,0 +1,6 @@
+<?php
+    $host = "host_name"; //TODO: add correct host name
+    $user = "dbusername"; //TODO: add user name that all users will use
+    $password = "user_password"; //TODO: add password for the main user account
+    $database = "db_name"; //TODO: add database name
+?>
